196d157df04eae87dd1a918cebe48b40f26c91e9
guojunsky <1006767658@qq.com>
2023-03-02 23:28:51 +0800

    fix 02

--- 2.js
+++ 2.js
@@ -14,53 +14,44 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
+
 var addTwoNumbers = function(l1, l2) {
-
-    let prev = null
-    let cur = l1
-    while(cur !== null) {
-        const t = cur.next
-        cur.next = prev
-        prev = cur
-        cur = t
-    }
-    let lnew1 = prev
-
-    prev = null
-    cur = l2
-    while(cur !== null) {
-        const t = cur.next
-        cur.next = prev
-        prev = cur
-        cur = t
-    }
-    let lnew2 = prev
-
-
-let carry = 1
-const head = new ListNode()
-cur = head
-    while(lnew1 !== null && lnew2 !== null) {
-        const result = lnew1.val + lnew2.val + carry
-        const node =  new ListNode(result % 10)
-        carry = parseInt(result/ 10)
+    const head = new ListNode()
+    let cur = head
+    let carry = 0
+    while(l1 !== null && l2 !== null) {
+        const result = l1.val + l2.val + carry
+        const node = new ListNode(result % 10)
+        carry = parseInt(result / 10)
         cur.next = node
         cur = node
-        lnew1 = lnew1.next
-        lnew2 = lnew2.next
+        l1 = l1.next
+        l2 = l2.next
     }
 
-    while(lnew1 !== null) {
-        const result = 
+    while (l1 !== null) {
+        const result = l1.val + carry
+        const node = new ListNode(result % 10)
+        carry = parseInt(result / 10)
+        cur.next = node
+        cur = node
+        l1 = l1.next
     }
 
-    cur = lnew1
-    let str = ''
-    while(cur !== null) {
-        str += cur.val + '->'
-        cur = cur.next
+    while (l2 !== null) {
+        const result = l2.val + carry
+        const node = new ListNode(result % 10)
+        carry = parseInt(result / 10)
+        cur.next = node
+        cur = node
+        l2 = l2.next
     }
 
-    console.log(str)
+    if (carry === 1) {
+        const node = new ListNode(1)
+        cur.next = node
+    }
 
+    return head.next
 };
